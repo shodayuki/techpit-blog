ce6be1c2d7e02daabcec4083f04b5ba7cc9be4b9
shodayuki <yshoda1227@gmail.com>
2021-11-19 11:36:38 +0900

    ナビゲーションを切り出し

--- post.php
+++ post.php
@@ -35,17 +35,7 @@
   <link rel="stylesheet" href="./css/blog.css">
 </head>
 <body>
-<nav class="navbar navbar-expand-md navbar-dark bg-red fixed-top">
-  <div class="container">
-    <a href="/" class="navbar-brand">My Blog Backend</a>
-    <div class="collapse navbar-collapse" id="navbarCollapse">
-      <ul class="navbar-nav me-auto mb-2 mb-md-0">
-        <li class="nav-item"><a href="#" class="nav-link">記事を書く</a></li>
-        <li class="nav-item"><a href="logout.php" class="nav-link">ログアウト</a></li>
-      </ul>
-    </div>
-  </div>
-</nav>
+<?php include('lib/nav.php'); ?>
 <main class="container">
   <div class="row">
     <div class="col-md-12">
