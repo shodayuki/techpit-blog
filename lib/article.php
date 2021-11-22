81aae267d1463a9abb7d039d54c44d76ee07af6f
shodayuki <yshoda1227@gmail.com>
2021-11-22 19:48:28 +0900

    filenameカラムを追加

--- lib/article.php
+++ lib/article.php
@@ -2,12 +2,13 @@
 
 class Article
 {
-  private $id;
-  private $title;
-  private $body;
-  private $category_id;
-  private $created_at;
-  private $updated_at;
+  private $id = null;
+  private $title = null;
+  private $body = null;
+  private $category_id = null;
+  private $filename = null;
+  private $created_at = null;
+  private $updated_at = null;
 
   public function save() {
     $queryArticle = new QueryArticle();
@@ -29,6 +30,10 @@ class Article
 
   public function getCategoryId() {
     return $this->category_id;
+  }
+
+  public function getFilename() {
+    return $this->filename;
   }
 
   public function getCreatedAt() {
@@ -53,6 +58,10 @@ class Article
 
   public function setCategoryId($category_id) {
     $this->category_id = $category_id;
+  }
+
+  public function setFilename($filename) {
+    $this->filename = $filename;
   }
 
   public function setCreatedAt($created_at) {
