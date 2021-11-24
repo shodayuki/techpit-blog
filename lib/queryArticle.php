639dea07dd5997e173eb3f77e0f8290bc1c75849
shodayuki <yshoda1227@gmail.com>
2021-11-24 11:46:59 +0900

    deleteメソッド修正

--- lib/queryArticle.php
+++ lib/queryArticle.php
@@ -88,10 +88,7 @@ class QueryArticle extends connect {
       // 新しいファイルがアップロードされたとき
       if ($file = $this->article->getFile()) {
         // ファイルが既にある場合、古いファイルを削除する
-        if ($this->article->getFilename()) {
-          unlink(__DIR__.'/../album/thumbs-'.$this->article->getFilename());
-          unlink(__DIR__.'/../album/'.$this->article->getFilename());
-        }
+        $this->deleteFile();
         // 新しいファイルのアップロード
         $this->article->setFilename($this->saveFile($file['tmp_name']));
         $filename = $this->article->getFilename();
@@ -119,8 +116,16 @@ class QueryArticle extends connect {
     }
   }
 
+  private function deleteFile(){
+    if ($this->article->getFilename()){
+      unlink(__DIR__.'/../album/thumbs-'.$this->article->getFilename());
+      unlink(__DIR__.'/../album/'.$this->article->getFilename());
+    }
+  }
+
   public function delete() {
     if ($this->article->getId()) {
+      $this->deleteFile();
       $id = $this->article->getId();
       $stmt = $this->dbh->prepare("UPDATE articles SET is_delete=1 WHERE id=:id");
       $stmt->bindParam(':id', $id, PDO::PARAM_INT);
