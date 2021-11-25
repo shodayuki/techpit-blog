5c6eb102b286ecb54da57ac567a64b0f96c3a30f
shodayuki <yshoda1227@gmail.com>
2021-11-25 14:40:59 +0900

    記事一覧の作成

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
+<?php
+  include 'lib/connect.php';
+  include 'lib/queryArticle.php';
+  include 'lib/article.php';
+
+  $queryArticle = new QueryArticle();
+  $articles = $queryArticle->findAll();
+?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -36,16 +44,23 @@
 <main class="container">
   <div class="row">
     <div class="col-md-8">
-      <article class="blog-post">
-        <h2 class="blog-post-title">記事タイトル</h2>
-        <p class="blog-post-meta">2021/xx/xx</p>
-        <p>本文がここに入ります。</p>
-      </article>
-      <article class="blog-post">
-        <h2 class="blog-post-title">記事タイトル2</h2>
-        <p class="blog-post-meta">2021/xx/xx</p>
-        <p>本文がここに入ります。</p>
-      </article>
+      <?php if ($articles): ?>
+        <?php foreach ($articles as $article): ?>
+          <article class="blog-post">
+            <h2 class="blog-post-title">
+              <a href="view.php?id=<?php echo $article->getId() ?>">
+                <?php echo $article->getTitle() ?>
+              </a>
+            </h2>
+            <p class="blog-post-meta"><?php echo $article->getCreatedAt() ?></p>
+            <?php echo nl2br($article->getBody()) ?>
+          </article>
+        <?php endforeach ?>
+      <?php else: ?>
+        <div class="alert alert-success">
+          <p>記事はありません。</p>
+        </div>
+      <?php endif ?>
     </div>
     <div class="col-md-4">
       <div class="p-4 mb-3 bg-light rounded">
