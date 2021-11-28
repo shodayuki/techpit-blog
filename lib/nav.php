4f7131108cd62749900d571de41bf67483c65e30
shodayuki <yshoda1227@gmail.com>
2021-11-28 18:45:46 +0900

    ナビゲーションにカテゴリーを追加

--- lib/nav.php
+++ lib/nav.php
@@ -3,6 +3,7 @@
     <a href="/" class="navbar-brand">My Blog Backend</a>
     <div class="collapse navbar-collapse" id="navbarCollapse">
       <ul class="navbar-nav me-auto mb-2 mb-md-0">
+        <li class="nav-item"><a href="category.php" class="nav-link">カテゴリー</a></li>
         <li class="nav-item"><a href="#" class="nav-link">記事を書く</a></li>
         <li class="nav-item"><a href="logout.php" class="nav-link">ログアウト</a></li>
       </ul>
