43b74cd91fad701ea96a1bf847aec36b31265698
shodayuki <yshoda1227@gmail.com>
2022-01-15 09:57:35 +0900

    カテゴリーの列を追加

--- backend.php
+++ backend.php
@@ -16,7 +16,7 @@
   $queryArticle = new QueryArticle();
   $pager = $queryArticle->getPager($page, $limit);
   $queryCategory = new QueryCategory();
-  $pager = $queryCategory->findAll();
+  $categories = $queryCategory->findAll();
 ?>
 <!DOCTYPE html>
 <html lang="ja">
@@ -65,6 +65,7 @@
               <th>タイトル</th>
               <th>本文</th>
               <th>画像</th>
+              <th>カテゴリー</th>
               <th>作成日</th>
               <th>更新日</th>
               <th>編集</th>
@@ -78,7 +79,7 @@
                 <td><?php echo $article->getTitle() ?></td>
                 <td><?php echo $article->getBody() ?></td>
                 <td><?php echo $article->getFilename()? '<img src="./album/thumbs-'.$article->getFilename().'">': 'なし' ?></td>
-                <td><?php echo isset($categories[$article->getCategoryId()]? $categories[$article->getCategoryId()]->getName(): 'なし' ?></td>
+                <td><?php echo isset($categories[$article->getCategoryId()])? $categories[$article->getCategoryId()]->getName(): 'なし' ?></td>
                 <td><?php echo $article->getCreatedAt() ?></td>
                 <td><?php echo $article->getUpdatedAt() ?></td>
                 <td><a href="edit.php?id=<?php echo $article->getId() ?>" class="btn btn-success">編集</a></td>
