94f55265b2a8d812c25c7607982153112b9dc400
shodayuki <yshoda1227@gmail.com>
2022-01-15 15:34:06 +0900

    削除ボタンを表示

--- category.php
+++ category.php
@@ -21,6 +21,12 @@
       $category->setName($_POST['name']);
       $category->save();
     }
+  } else if (!empty($_GET['action']) && $_GET['action'] == 'delete' && !empty($_GET['id'])) {
+    // 削除モードのとき
+    $category = $queryCategory->find($_GET['id']);
+    if ($category) {
+      $category->delete();
+    }
   }
 
   // 登録されているカテゴリーをすべて取得
@@ -97,6 +103,7 @@
               <th>ID</th>
               <th>カテゴリー名</th>
               <th>編集</th>
+              <th>削除</th>
             </tr>
           </thead>
           <tbody>
@@ -105,6 +112,7 @@
                 <td><?php echo $c->getId() ?></td>
                 <td><?php echo $c->getName() ?></td>
                 <td><a href="category.php?action=edit&id=<?php echo $c->getId() ?>" class="btn btn-success">編集</a></td>
+                <td><a href="category.php?action=delete&id=<?php echo $c->getId() ?>" class="btn btn-danger">削除</a></td>
               </tr>
             <?php endforeach ?>
           </tbody>
